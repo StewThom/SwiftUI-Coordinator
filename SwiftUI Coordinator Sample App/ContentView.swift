c7e70ea6047f6340f63ff003a6aeb75ee37564d1
Stewart Thomson <Stew12Thom@gmail.com>
2023-09-26 14:34:53 +1000

    Initial Commit

---
+++ SwiftUI Coordinator Sample App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Coordinator Sample App
+//
+//  Created by Stewart Thomson on 26/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Coordinator Sample App/SwiftUI_Coordinator_Sample_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Coordinator_Sample_AppApp.swift
+//  SwiftUI Coordinator Sample App
+//
+//  Created by Stewart Thomson on 26/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Coordinator_Sample_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
